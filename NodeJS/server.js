2e76cbe5acc10823df1ec37704920ca2eeac397a
Zeroska <owlthres@gmail.com>
2019-11-22 13:53:05 +0700

    *install express and socket.io

--- NodeJS/server.js
+++ NodeJS/server.js
@@ -2,34 +2,29 @@
   #connection cua ras voi server la thannh log roi truyen len firebase
   #*/
 
-const app = require('express')();
-const server =  require('http').Server(app);
+//create a http server 
+const app = require('express')();  
+const server =  require('http').Server(app); 
+//add socket io to the server 
 const io = require('socket.io')(server);
 
-server.listen(80);
+server.listen(8000,'127.0.0.1'); //listen for connection 
 
+
+//Restful api
 app.get('/',(req,res)=>{
-  
+  res.send("<h1>Hello you fucking piece of shit</h1>") 
 });
 
 //RasPi need to talk to the server on port 80 
-
 //First build the handler for the incoming connection for the RasPi
-
-
-
-
-
-
-
-
 
 
 io.on('connection',(socket)=>{ //when RasPi connected to port 80 
   console.log("RasPi has connected")
   socket.emit('testConnection', {description: 'Hi! RasPi has connected to the server'}); 
   socket.on('RasPiRequest', (data, msg)=>{
-    
+             
   });  
 });
 
