aa66fe6db5512bdf159dd4b7c493c96e9176a515
truonghoangduy <hoangduy@Hoangs-MacBook-Pro.local>
2019-11-23 20:51:29 +0700

    Refactor + create sample code

--- flutter-app/smart_garden/lib/wiget/showText.dart
+++ flutter-app/smart_garden/lib/wiget/showText.dart
@@ -1,48 +1,125 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_database/firebase_database.dart';
 
 class showTextWithOption extends StatelessWidget {
-  final String vaule;
+  FirebaseDatabase database = FirebaseDatabase(databaseURL: 'https://iot-thcntt1.firebaseio.com');
   final specialChar;
   final String notation;
-  GlobalKey vaule_key = GlobalKey();
-  showTextWithOption(this.vaule, this.specialChar, this.notation);
+  final String db_Path;
+  final double textScale=50;
+  final text_color= Color.fromRGBO(112, 112, 112, 1.0);
+  showTextWithOption(this.db_Path,this.specialChar, this.notation);
   @override
   Widget build(BuildContext context) {
-    double spacing = 30;
-    bool overflowVaule_dectector = false;
-    if (this.vaule.length >= 3) {
-      overflowVaule_dectector = true;
+    bool istrigger=false;
+    bool flag = false;
+    if (this.specialChar=="o") {
+      flag= true;
     }
-
-    // TODO: implement build
-    return Container(
-        // padding: EdgeInsets.all(0),
-        // alignment: Alignment.center,
-        width: 150,
-        height: 45,
-        // color: Colors.blueGrey,
-        child: Stack(children: <Widget>[
-          Positioned(
-            key: vaule_key,
-            // top: 20,
-            child: Text(
-              this.vaule,
-              style: TextStyle(fontSize: 40, fontWeight: FontWeight.w200),
-            ),
-          ),
-          Positioned(
-              left: overflowVaule_dectector ? spacing + 40 : 50,
-              // top: 20,
-              child: Text(
-                this.specialChar,
-              )),
-          Positioned(
-              left: overflowVaule_dectector ? spacing + 55 : 64,
-              // top: 20,
-              child: Text(
-                this.notation,
-                style: TextStyle(fontSize: 40, fontWeight: FontWeight.w200),
-              ))
-        ]));
-  }
+    return StreamBuilder<Event>(
+        // stream: callbackonchange.asBroadcastStream(),
+        stream: FirebaseDatabase.instance
+            .reference()
+            .child(this.db_Path)
+            .onValue,
+        builder: (context, snap) {
+          if (snap.connectionState == ConnectionState.active) {
+            istrigger=!istrigger;
+            print(snap.data);
+            print(snap.data.snapshot.value);
+            var data = snap.data.snapshot.value.toString();
+            var data_lenght = data.length;
+            print(data_lenght);
+          return Container(
+                width: 173,
+                height: 70,
+                decoration: BoxDecoration(
+                  color: Color.fromRGBO(255,255,255,1.0),
+                  borderRadius: BorderRadius.circular(12)
+                ),
+                child: Padding(
+                  padding: EdgeInsets.only(left: 10,right: 10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      AnimatedDefaultTextStyle(
+                        style: istrigger?
+                        TextStyle(
+                          fontSize: textScale,
+                          color: text_color,
+                          fontWeight: FontWeight.w200
+                        )
+                        :TextStyle(
+                          fontSize: textScale,
+                          color: text_color,
+                          fontWeight: FontWeight.w200
+                        ),
+                        duration: Duration(microseconds: 300),
+                        curve: Curves.linear,
+                        child:Text(data)
+                      ),
+                      // Text(data,style: TextStyle(fontSize: textScale, fontWeight: FontWeight.w200)),
+                      Container(
+                        width: 15,
+                        child: Stack(
+                          children: <Widget>[
+                            Positioned(
+                        top: 6,
+                        left: flag?3:0,
+                          child: Align(
+                            alignment: Alignment.centerRight,
+                            child: Text(
+                              this.specialChar,
+                            ),
+                          )),
+                          ],
+                        ),
+                      ),
+                      Text(notation,style: TextStyle(fontSize: textScale, fontWeight: FontWeight.w200))
+                    ],
+                  ),
+                ));
+                // child: Container(
+                //   child: Stack(children: <Widget>[
+                //     Positioned(
+                //       left: 10,
+                //       child: Text(
+                //         data,
+                //         // '123456',
+                //         style:
+                //             TextStyle(fontSize: 50, fontWeight: FontWeight.w200),
+                //       ),
+                //     ),
+                //     Positioned(
+                //       top: 6,
+                //         // left: specailchar_spacing,
+                //         // top: 20,
+                //         child: Align(
+                //           alignment: Alignment.centerRight,
+                //           child: Text(
+                //             this.specialChar,
+                //           ),
+                //         )),
+                //     Positioned(
+                //         // left: notation_spacing,
+                //         // top: 20,
+                //         child: Align(
+                //           alignment: Alignment.centerRight,
+                //           child: Text(
+                //             this.notation,
+                //             style: TextStyle(
+                //                 fontSize: 50, fontWeight: FontWeight.w200),
+                //           ),
+                //         ))
+                //   ]
+                //   ),
+                // )
+          }
+           else{
+            return Text("Not Functioning");
+          }
+        });
+  
+        }
 }
---
+++ flutter-app/smart_garden/lib/wiget/stream_showText.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
+
+class streamshowTextWithOption extends StatelessWidget {
+  FirebaseDatabase database = FirebaseDatabase(
+    databaseURL: 'https://iot-thcntt1.firebaseio.com'
+  );
+  // final String vaule;
+  // final specialChar;
+  // final String notation;
+  // GlobalKey vaule_key = GlobalKey();
+  // showTextWithOption(this.vaule, this.specialChar, this.notation);
+  @override
+  Widget build(BuildContext context) {
+    // double spacing = 30;
+    // bool overflowVaule_dectector = false;
+    // if (this.vaule.length >= 3) {
+    //   overflowVaule_dectector = true;
+    // }
+
+    // TODO: implement build
+    return StreamBuilder<Event>(
+              // stream: callbackonchange.asBroadcastStream(),
+              stream: FirebaseDatabase.instance.reference().child('user/linh/temp').onValue,
+              builder: (context, snap) {
+                if (snap.connectionState==ConnectionState.active) {
+                  print(snap.data);
+                  print(snap.data.snapshot.value);
+                  return Container(
+                    width: 50,
+                    height: 50,
+                    color: Colors.yellow,
+                    child: Text(snap.data.snapshot.value.toString(),textAlign: TextAlign.center,),
+                  );
+                } else {
+                  print('notworking');
+                  print(snap.data.toString());
+                  return Text("No data");
+                }
+              },
+            );
+
+
+  }
+
+  
+}
