ea3a05461fba54f02a84e8bcad46723800a8f692
truonghoangduy <hoangduy@Hoangs-MacBook-Pro.local>
2019-11-23 20:50:54 +0700

    test socket io to gcloud hosting

---
+++ flutter-app/smart_garden/lib/wiget/socket.dart
@@ -0,0 +1,57 @@
+import 'dart:ffi';
+
+// import 'package:socket_io/socket_io.dart';
+import 'package:socket_io_client/socket_io_client.dart' as IO;
+
+void  main() async{
+  IO.Socket socket = IO.io('http://35.213.149.38:8000', <String, dynamic>{
+    'transports': ['websocket'],
+  });
+  socket.id="duybeo";
+  socket.on('connect', (mesg)=>{
+    print(mesg)
+  });
+    socket.on("duy", (handler)=>{
+    print(handler)
+  });
+      socket.on("duybeo", (handler)=>{
+    print(handler)
+  });
+
+  // Future<Void> aaa(){
+    
+  //   return "AAAA";
+  // }
+}
+  // socket.on("duy", (handler)=>{
+  //   print(handler)
+  // });
+//   socket.on("duybeo", (handler)=>{
+//     print(handler)
+//   });
+//   socket.emit("love",{
+//     'aaa':1111,
+//     "2222":2222,
+//   });
+// }
+
+// class socket_IO{
+  
+  
+
+  
+  // IO.Socket socket = IO.io('http://localhost:3000', <String, dynamic>{
+  //   'transports': ['websocket'],
+  // });
+  //  ;
+
+  
+
+
+  
+  
+  
+  
+  
+
+// }
