f394cf4729e3d9329ca16995e17664cfaf3ea586
Zeroska <owlthres@gmail.com>
2019-11-23 14:38:37 +0700

    Adding the socket.io-client for testing purpose

--- NodeJS/public/client.js
+++ NodeJS/public/client.js
@@ -1,4 +1,14 @@
-const socket 
+
+const socket = require('socket.io-client')('http://127.0.0.1:8000')
+
+setInterval(()=>{
+    socket.emit('DataToServer',{ 'dataPayLoad':'Testing information',})
+},3000)
 
 
-let socket.io = 
+
+
+
+
+
+
